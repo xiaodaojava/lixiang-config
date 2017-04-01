6aab61be8d06728aeba05785af1b015fa6f29125
lixiang <good396975802@qq.com>
2017-04-01 23:32:44 +0800

    添加了一个事件响应的demo

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,16 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+    @FXML
+    private Button submitPrint;
+
+    @FXML
+    private void submitPrintMethod(ActionEvent event){
+        System.out.println("submitPrint has been clicked");
+    }
 
 }
