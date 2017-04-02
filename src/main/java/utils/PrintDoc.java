fe04083d50ae5e7cca195fd3c1cbbcc9094e1bcb
smileknife <1506500889@qq.com>
2017-04-02 20:58:31 +0800

    添加了打印的代码，经过测试能够在windows下运行

---
+++ src/main/java/utils/PrintDoc.java
@@ -0,0 +1,42 @@
+package utils;
+
+import com.jacob.activeX.ActiveXComponent;
+import com.jacob.com.ComThread;
+import com.jacob.com.Dispatch;
+import com.jacob.com.Variant;
+
+
+
+/**
+ * Created by Administrator on 2017/4/2.
+ */
+public class PrintDoc {
+    public static void main(String[] args) {
+        String path="D:\\simple.docx";
+        System.out.println("开始打印");
+        ComThread.InitSTA();
+        ActiveXComponent word=new ActiveXComponent("Word.Application");
+        Dispatch doc=null;
+        Dispatch.put(word, "Visible", new Variant(false));
+        Dispatch docs=word.getProperty("Documents").toDispatch();
+        doc=Dispatch.call(docs, "Open", path).toDispatch();
+
+        try {
+            Dispatch.call(doc, "PrintOut");//打印
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("打印失败");
+        }finally{
+            try {
+                if(doc!=null){
+                    Dispatch.call(doc, "Close",new Variant(0));
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+            //释放资源
+            ComThread.Release();
+        }
+
+    }
+}
