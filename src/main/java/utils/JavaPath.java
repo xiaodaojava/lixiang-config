5ea5e1177b0f1db5257af5c4a865c8713ea882e0
lixiang <lixiang9409@vip.qq.com>
2017-04-27 10:46:44 +0800

    开始尝试用freemark来生成word文档，排版样式会相对固定些

---
+++ src/main/java/utils/JavaPath.java
@@ -0,0 +1,10 @@
+package utils;
+
+/**
+ * Created by lixiang on 26/04/2017.
+ */
+public class JavaPath {
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("java.library.path"));
+    }
+}
--- src/main/java/utils/PrintDoc.java
+++ src/main/java/utils/PrintDoc.java
@@ -12,31 +12,37 @@ import com.jacob.com.Variant;
  */
 public class PrintDoc {
     public static void main(String[] args) {
-        String path="D:\\simple.docx";
+        String path="C:\\test\\simple.docx";
         System.out.println("开始打印");
-        ComThread.InitSTA();
-        ActiveXComponent word=new ActiveXComponent("Word.Application");
-        Dispatch doc=null;
-        Dispatch.put(word, "Visible", new Variant(false));
-        Dispatch docs=word.getProperty("Documents").toDispatch();
-        doc=Dispatch.call(docs, "Open", path).toDispatch();
-
-        try {
-            Dispatch.call(doc, "PrintOut");//打印
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("打印失败");
-        }finally{
+        try{
+            ComThread.InitSTA();
+            ActiveXComponent word=new ActiveXComponent("Word.Application");
+            Dispatch doc=null;
+            Dispatch.put(word, "Visible", new Variant(false));
+            Dispatch docs=word.getProperty("Documents").toDispatch();
+            doc=Dispatch.call(docs, "Open", path).toDispatch();
             try {
-                if(doc!=null){
-                    Dispatch.call(doc, "Close",new Variant(0));
+                Dispatch.call(doc, "PrintOut");//打印
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("打印失败");
+            }finally{
+                try {
+                    if(doc!=null){
+                        Dispatch.call(doc, "Close",new Variant(0));
+                    }
+                } catch (Exception e2) {
+                    e2.printStackTrace();
                 }
-            } catch (Exception e2) {
-                e2.printStackTrace();
+                //释放资源
+                ComThread.Release();
             }
-            //释放资源
-            ComThread.Release();
+
+        }catch (Exception e){
+            e.printStackTrace();
         }
+
+
 
     }
 }
