30093f52e0f26ad2787d92ccde79fba48ff493b8
lixiang <lixiang9409@vip.qq.com>
2018-09-21 14:58:35 +0800

    添加了gitignore

--- src/main/java/red/lixiang/se/Main.java
+++ src/main/java/red/lixiang/se/Main.java
@@ -4,6 +4,9 @@ package red.lixiang.se;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * 启动参数添加： --module-path /Users/lixiang/soft/javafx-sdk-11/lib --add-modules=javafx.controls
+ */
 public class Main extends Application {
 
     public static void main(String[] args) {
