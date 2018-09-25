3b9db9d006cf74d4c9ae04aad5fb055d99b886d9
lixiang <lixiang9409@vip.qq.com>
2018-09-25 21:00:13 +0800

    更新

--- src/main/java/red/lixiang/se/Main.java
+++ src/main/java/red/lixiang/se/Main.java
@@ -2,10 +2,16 @@ package red.lixiang.se;
 
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 /**
- * 启动参数添加： --module-path /Users/lixiang/soft/javafx-sdk-11/lib --add-modules=javafx.controls
+ * 启动参数添加： --module-path /Users/lixiang/soft/javafx-sdk-11/lib
+ * --add-modules=javafx.base,javafx.controls,javafx.fxml,javafx.graphics,javafx.media,javafx.swing,javafx.swt,javafx.web
  */
 public class Main extends Application {
 
@@ -14,7 +20,11 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+
+        Scene scene = new Scene(root, 300, 275);
+        stage.setScene(scene);
         stage.show();
     }
 }
---
+++ src/main/java/red/lixiang/se/controller/MainController.java
@@ -0,0 +1,8 @@
+package red.lixiang.se.controller;
+
+/**
+ * @Author lixiang
+ * @CreateTime 2018/9/25
+ **/
+public class MainController {
+}
